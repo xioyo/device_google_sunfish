84425ce98a9cff2aa490993132c74be00e524d17
ucoboy <sufiyanmp07@gmail.com>
2022-12-05 11:28:39 +0000

    sunfish: CipherOS Bringup

--- cipher_sunfish.mk
+++ cipher_sunfish.mk
@@ -4,11 +4,13 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit some common xdroid stuff.
-$(call inherit-product, vendor/xdroid/config/common.mk)
+# Inherit some common cipherOS stuff.
+$(call inherit-product, vendor/cipher/config/common_full_phone.mk)
 TARGET_SUPPORTS_QUICK_TAP := true
-XDROID_MAINTAINER := xioyo
-XDROID_BOOT := 1080
+CIPHER_MAINTAINER := xioyo
+CIPHER_GAPPS := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_BOOT_ANIMATION_RES := 1080
 
 # Inherit device configuration
 $(call inherit-product, device/google/sunfish/aosp_sunfish.mk)
@@ -18,7 +20,7 @@ include device/google/sunfish/device-lineage.mk
 # Device identifier. This must come after all inclusions
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Pixel 4a
-PRODUCT_NAME := xdroid_sunfish
+PRODUCT_NAME := cipher_sunfish
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT=sunfish \
